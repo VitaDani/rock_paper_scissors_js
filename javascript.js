659392e703ad5702e05992268ffe90830792014a
Daniel Vitacolonna <44033488+VitaDani@users.noreply.github.com>
2024-07-31 22:06:43 -0600

    Add DOM manipulation to display the round
    Initialized the scores for both players, and put the display for the user to know who wins the round.

--- javascript.js
+++ javascript.js
@@ -33,35 +33,37 @@ container.addEventListener('click', (event) => {
     }
 });
 
+// Create the query that lets the user know who wins the current round
+let winner = document.querySelector("#winner");
+
 // Create the function that allows a round of the game to be played
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice;
     computerChoice = getComputerChoice();
 
     if (humanChoice == computerChoice) {
-        console.log("This round is a draw.");
+        winner.textContent = `This round is a draw. Both players picked ${humanChoice}`;        
     } else if (humanChoice == 'rock' && computerChoice == 'paper') {
-        console.log("The computer wins this round. Paper beats Rock!")
+        winner.textContent = "The computer wins this round. Paper beats Rock!";        
         computerScore++;
     } else if (humanChoice == 'rock' && computerChoice == 'scissors') {
-        console.log("The player wins this round. Rock beats Scissors!");
+        winner.textContent = "The player wins this round. Rock beats Scissors!";        
         humanScore++;
     } else if (humanChoice == 'paper' && computerChoice == 'rock') {
-        console.log("The player wins this round. Paper beats Rock!");
+        winner.textContent = "The player wins this round. Paper beats Rock!";        
         humanScore++;
     } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
-        console.log("The computer wins this round. Scissors beats Paper!");
+        winner.textContent = "The computer wins this round. Scissors beats Paper!";        
         computerScore++;
     } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
-        console.log("The computer wins this round. Rock beats Scissors!");
+        winner.textContent = "The computer wins this round. Rock beats Scissors!";        
         computerScore++;
     } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
-        console.log("The player wins this round. Scissors beats Paper!");
+        winner.textContent = "The player wins this round. Scissors beats Paper!";
         humanScore++;
     }
-    h_score.textContent = `${humanScore}`;
-    c_score.textContent = `${computerScore}`;
-    console.log(`The computer has a score of ${computerScore} and the player has a score of ${humanScore}`);
+    h_score.textContent = humanScore;
+    c_score.textContent = computerScore;
 }
 
 // Create two variables to keep the score for each player
@@ -72,8 +74,9 @@ let computerScore = 0;
 let human_score = document.querySelector("#human_score");
 let computer_score = document.querySelector("#computer_score");
 let h_score = document.createElement("p");
+h_score.textContent = 0;
 let c_score = document.createElement("p");
-
+c_score.textContent = 0;
 
 h_score.style.textAlign = "center";
 c_score.style.textAlign = "center";
